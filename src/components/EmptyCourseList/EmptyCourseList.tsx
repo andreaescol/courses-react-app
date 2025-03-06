3fea4ba7b8ba8f32bed310b4b3393a2a7d207c00
andrea <omandrea7@gmail.com>
2025-03-06 11:16:24 -0500

    Update EmptyCourseList to behave depending on ADMIN or REGULAR user.

--- src/components/EmptyCourseList/EmptyCourseList.tsx
+++ src/components/EmptyCourseList/EmptyCourseList.tsx
@@ -1,16 +1,30 @@
+import { useSelector } from "react-redux";
 import Button from "../../common/Button/Button";
 import styles from "./emptyCourseList.module.css";
+import { getUser } from "../../store/selectors";
+import { useNavigate } from "react-router-dom";
 
 const EmptyCourseList = () => {
+  const navigate = useNavigate();
+  const isAdmin = useSelector(getUser).role === "ADMIN";
+
   return (
     <div className={styles.container}>
       <h1>Your List is Empty</h1>
-      <p>Please use "Add New Course" button to add your first course</p>
-      <Button
-        buttonText="Add New Course"
-        onClick={() => {}}
-        className="btn btn-info"
-      />
+      {isAdmin === true ? (
+        <>
+          <p>Please use "Add New Course" button to add your first course</p>
+          <Button
+            buttonText="Add New Course"
+            onClick={() => navigate("/courses/add")}
+            className="btn btn-info"
+          />
+        </>
+      ) : (
+        <p>
+          You don't have permissions to create a course. Please log in as ADMIN.
+        </p>
+      )}
     </div>
   );
 };
